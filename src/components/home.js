91e051ad3657bec25d1e0171234ab3fe807af637
Gary <miskhill@hotmail.com>
2024-10-14 11:24:13 +0100

    feat: Initial transfer of files for frontend

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ImportCard from "./importCard.js";
+
+function Home() {
+  return (
+    <div className='home-background'>
+      <ImportCard />
+    </div>
+  );
+}
+
+export default Home;
